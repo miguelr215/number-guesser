f95286c80b2472f94f5d85eb9263cb5ac431c354
miguelr215 <66912426+miguelr215@users.noreply.github.com>
2020-06-30 05:30:32 -0700

    Initial upload

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+GAME FUNCTION:
+- Player must guess a number betwee a min and max
+- Player gets a certer amount of guesses
+- Notify player of guesses remaining
+- Notify the player of the correct answer if lose
+- Let player choose to play again
+*/
+
+
+
